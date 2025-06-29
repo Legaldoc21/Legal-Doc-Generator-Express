0fb3b57596733bf67444b414b9c671cf65117cb1
Legaldoc21 <legaldocai0@gmail.com>
2025-06-29 20:44:22 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '4rem' }}>
+      <h1>Redactador Legal Exprés</h1>
+      <p>Tu documento legal en menos de 2 minutos</p>
+    </div>
+  );
+}
