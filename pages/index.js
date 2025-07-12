6c3442accdcf93583a06453810b318a735025d0a
Legaldoc21 <legaldocai0@gmail.com>
2025-07-12 13:44:42 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-900 p-4">
-      <h1 className="text-4xl font-bold mb-4">Redactor Legal Exprés</h1>
-      <p className="text-lg">Describe tu situación legal y genera un documento personalizado.</p>
-    </div>
-  );
-}
